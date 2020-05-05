891eb67d469ae7a83850ac57444288b3b3407377
sys <2506715686@qq.com>
2020-05-05 11:54:41 +0800

    1

--- src/main/java/com/sys/api/ApiApplication.java
+++ src/main/java/com/sys/api/ApiApplication.java
@@ -2,12 +2,32 @@ package com.sys.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ApiApplication.class, args);
+
+//        ConfigurableApplicationContext context =
+//
+//                SpringApplication.run(ApiApplication.class, args);
+//
+//        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+//
+//        List<Map<String, Object>> result =
+//
+//                jdbcTemplate.queryForList("SELECT * FROM USER");
+//
+//        System.out.println(result);
+
+
     }
 
 }
